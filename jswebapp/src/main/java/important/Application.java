7f872c9b894ab96b11cb96c940d13abfed63a4f4
flvipn <pauliepana92@gmail.com>
2025-02-26 22:45:42 +0200

    Add files via upload

---
+++ jswebapp/src/main/java/important/Application.java
@@ -0,0 +1,13 @@
+package important;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class git initApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
